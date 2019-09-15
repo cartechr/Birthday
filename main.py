106a2d7deed467b421101ee930e3d351e22f4f27
Christian <cartechr@iu.edu>
2019-09-15 13:53:10 -0400

    save

---
+++ main.py
@@ -0,0 +1,14 @@
+import json
+birthday_dictionary = {
+	"Carter": 5/4/2001,
+	"Simon": 9/28/1999,
+	"Cameron": 1/16/2002,
+	"Gabi": 8/12/2001,
+    "Jay" : 6/23/2001,
+    "Sebastian" : 1/30/2000,
+    "Kate" : 10/10/2004
+}
+
+user = ' '
+print ('>>> Welcome to the birthday dictionary. We know the birthdays of: \nCarter \nSimon \nCameron \nGabi \nJay \nSebastian \nKate')
+user = input("Who's birthday would you like to know?") 
