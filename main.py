1e29bbd230f4120b391bd4250b9e5307b4189135
Carter <cartechr@iu.edu>
2019-09-16 18:04:12 -0400

    finished

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-import json
+
 birthday_dictionary = {
-	"Carter": 5/4/2001,
-	"Simon": 9/28/1999,
-	"Cameron": 1/16/2002,
-	"Gabi": 8/12/2001,
-    "Jay" : 6/23/2001,
-    "Sebastian" : 1/30/2000,
-    "Kate" : 10/10/2004
+	"Carter": "5/4/2001",
+	"Simon": "9/28/1999",
+	"Cameron": "1/16/2002",
+	"Gabi": "8/12/2001",
+    "Jay" : "6/23/2001",
+    "Sebastian" : "1/30/2000",
+    "Kate" : "10/10/2004"
 }
 
-user = ' '
+
 print ('>>> Welcome to the birthday dictionary. We know the birthdays of: \nCarter \nSimon \nCameron \nGabi \nJay \nSebastian \nKate')
-user = input("Who's birthday would you like to know?")
+print("\nWho's birthday do you want to look up?")
+name = input()
+if name in birthday_dictionary:
+    print('{}\'s birthday is {}.'.format(name, birthday_dictionary[name]))
+else:
+	print('Sorry not in the dictionary :(')
